320ed1a7ca396b52a2f4c1ce0d67292ca8f5aa4f
Prasad Ande <andeprasad786@gmail.com>
2025-03-25 09:07:34 +0530

    Return true if Duplicate element present in Array

---
+++ LeetCode/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        // int arr[] = {1, 2, 3, 1};
+        // int arr[] = {1, 2, 3, 4};
+        int arr[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println("The Array contains Duplicate : " + containsDuplicate(arr));
+    }
+    static public boolean containsDuplicate(int[] arr)
+    {
+        List<Integer> listcheck = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(listcheck.contains(arr[i]))
+            {
+                return true;
+            }
+            else
+            {
+                listcheck.add(arr[i]);
+            }
+        }
+        return false;
+    }
+}
